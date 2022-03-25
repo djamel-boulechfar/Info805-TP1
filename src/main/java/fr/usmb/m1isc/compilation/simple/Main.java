5df68c3d5295516332859bb3a7d19287b47c3f90
Djamel <djamelboulechfar@gmail.com>
2022-03-25 11:15:33 +0100

    Début exercice 1

--- src/main/java/fr/usmb/m1isc/compilation/simple/Main.java
+++ src/main/java/fr/usmb/m1isc/compilation/simple/Main.java
@@ -5,13 +5,22 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SimpleLexer lexer;
+        /*SimpleLexer lexer;
         if (args.length > 0)
             lexer = new SimpleLexer(new FileReader(args[0]));
         else
             lexer = new SimpleLexer(new InputStreamReader(System.in));
         @SuppressWarnings("deprecation")
         SimpleParser p = new SimpleParser(lexer);
+        p.parse();*/
+
+        ExpressionsArithmetiquesLexer expLexer;
+        if (args.length > 0)
+            expLexer = new ExpressionsArithmetiquesLexer(new FileReader(args[0]));
+        else
+            expLexer = new ExpressionsArithmetiquesLexer(new InputStreamReader(System.in));
+        @SuppressWarnings("deprecation")
+        ExpressionsArithmetiquesParser p = new ExpressionsArithmetiquesParser(expLexer);
         p.parse();
     }
 }
